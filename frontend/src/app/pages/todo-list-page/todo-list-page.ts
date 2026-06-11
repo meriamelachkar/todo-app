d31d14b3d47a62bf990891c4229c9bb42f51600e
meriam <meriamelachkar@gmail.com>
2026-06-11 16:05:04 +0200

    chore: add frontend components

---
+++ frontend/src/app/pages/todo-list-page/todo-list-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-page',
+  imports: [],
+  templateUrl: './todo-list-page.html',
+  styleUrl: './todo-list-page.css',
+})
+export class TodoListPage {}
